ace82bc08b9dff3bfe74c19ea6ba8a09c33b7a8f
Romance Cox <heyycurlie@gmail.com>
2023-01-30 16:29:15 -0500

    lesson linear

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,29 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
+import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
-	}
+		System.out.print("0,1,");
 
+		int upTo = 21;
+
+		int firstVal = 0;
+		int secondVal = 1;
+
+		for (int i = 0; secondVal < upTo; i++) {
+
+			int newVal = firstVal + secondVal;
+
+			Systemsystem.out.print(newVal + ", ");
+
+			firstVal = secondVal;
+			secondVal = newVal;
+		}
+
+	}
 }
--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,32 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
 
-	}
+		System.out.println("Please enter the (temperature");
+		Scanner input = new Scanner(System.in);
 
+		String temperatureStr = input.nextLine();
+		int temperatureInt = Integer.parseInt(temperatureStr);
+		temperatureInt = Integer.parseInt(temperatureStr);
+
+		System.out.println("Is the temperature in (C)elsius, or (F)ahrenheit?)");
+		String optionSelected = input.nextLine();
+
+		if (optionSelected.equals("C")) {
+			double temperatureFahrenheit = temperatureInt * 1.8 + 32;
+
+			System.out.println(temperatureStr + "C is" + temperatureFahrenheit);
+
+			if (optionSelected.equals("F")) {
+				double temperatureCelsius = (temperatureInt - 32) / 1.8;
+
+				System.out.println(temperatureStr + "F is" + temperatureCelsius);
+
+			}
+		}
+	}
 }
