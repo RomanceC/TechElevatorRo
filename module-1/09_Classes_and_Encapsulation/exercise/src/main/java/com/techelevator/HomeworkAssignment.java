dc7721de6552301c59869dd5ce135b98e0b1beac
Romance Cox <heyycurlie@gmail.com>
2023-02-02 17:24:54 -0500

    lesson tv

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,46 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
+    private String letterGrade;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this. submitterName = submitterName;
+    }
+    public int getEarnedMarks(){
+        return earnedMarks;
+    }
+    public int getPossibleMarks(){
+    return possibleMarks;
+    }
+    public String getSubmitterName() {
+        return submitterName;
+    }
+    public String getLetterGrade(){
+    double percentScore = getEarnedMarks()* 100 / getPossibleMarks();
+
+    if (percentScore >= 90) {
+            letterGrade = "A";
+        } else if (percentScore >= 80 && percentScore <= 89) {
+            letterGrade = "B";
+        } else if (percentScore >= 70 && percentScore <= 79) {
+            letterGrade = "C";
+        } else if (percentScore >= 60 && percentScore <= 69) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+        return letterGrade;
+    }
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+
+    //Setters
+    public void SetEarnedMarks(int earnedMarks){
+    this.earnedMarks = earnedMarks;
+    }
 }
