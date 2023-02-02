e8c74a9c5c927ea1920cfb592c409a6474a5fcb2
Romance Cox <heyycurlie@gmail.com>
2023-02-02 18:41:39 -0500

    lessont

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,11 +1,12 @@
 package com.techelevator;
 
-public class Elevator { private int currentFloor = 1;
+public class Elevator {
+    private int currentFloor = 1;
     private int numberOfFloors;
     private boolean doorOpen;
 
-    public Elevator(int totalNumberOffloors) {
-        this.numberOfFloors = totalNumberOffloors;
+    public Elevator(int totalNumberOfFloors) {
+        this.numberOfFloors = totalNumberOfFloors;
     }
 
     public void openDoor() {
@@ -17,7 +18,7 @@ public class Elevator { private int currentFloor = 1;
     }
 
     public void goUp(int desiredFloor) {
-        if (doorOpen != true){
+        if (doorOpen != true) {
             if (desiredFloor <= numberOfFloors) {
                 if (desiredFloor > currentFloor) {
                     currentFloor = desiredFloor;
@@ -28,25 +29,11 @@ public class Elevator { private int currentFloor = 1;
 
     public void goDown(int desiredFloor) {
         if (doorOpen != true) {
-            if (desiredFloor >=1 ) {
-                if (desiredFloor < currentFloor){
+            if (desiredFloor >= 1) {
+                if (desiredFloor < currentFloor) {
                     currentFloor = desiredFloor;
                 }
             }
         }
     }
-
-    public int getCurrentFloor() {
-        return currentFloor;
-    }
-
-    public int getNumberOfFloors() {
-        return numberOfFloors;
-    }
-
-    public boolean isDoorOpen() {
-        return doorOpen;
-    }
-
 }
-
--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -2,35 +2,72 @@ package com.techelevator;
 
 public class Television {
 
-    private boolean isOn = false;
-    private int currentChannel = 3;
-    private int currentVolume = 2;
+        private boolean isOn = false;
+        private int currentChannel = 3;
+        private int currentVolume =2;
 
-    public void turnOff() {
-        isOn = false;
-    }
+        //Constructors
 
-    public void turnOn() {
-        isOn = true;
-        currentChannel = 3;
-        currentVolume = 2;
-    }
 
-    public void changeChannel(int newChannel) {
-        if (isOn == true){
-            if (newChannel > 2 && newChannel < 19){
+
+        //Getters
+        public boolean isOn() {
+            return isOn;
+        }
+
+        public int getCurrentChannel() {
+            return currentChannel;
+        }
+
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+
+        //Methods
+
+        public void turnOff() {
+            isOn = false;
+        }
+
+        public void turnOn() {
+            isOn = true;
+        }
+
+        public void changeChannel(int newChannel) {
+            if (isOn && newChannel >= 3 && newChannel <= 18) {
                 currentChannel = newChannel;
             }
         }
-    }
 
-    public void channelUp() {
-        if (isOn == true) {
-            currentChannel++;
-            if (currentChannel > 18) {
-                currentChannel = 3;
+        public void channelUp() {
+            if (isOn) {
+                if (currentChannel == 18) {
+                    currentChannel = 3;
+                } else {
+                    currentChannel++;
+                }
+            }
+        }
+
+        public void channelDown() {
+            if (isOn) {
+                if (currentChannel == 3) {
+                    currentChannel = 18;
+                } else {
+                    currentChannel--;
+                }
+            }
+        }
+
+        public void raiseVolume() {
+            if (isOn && currentVolume < 10) {
+                currentVolume++;
+            }
+        }
+
+        public void lowerVolume() {
+            if (isOn && currentVolume > 0) {
+                currentVolume--;
             }
         }
     }
-
-}
