b72ff32de6cdc890ab4c399a0cc0bef6bbac28f1
Romance Cox <heyycurlie@gmail.com>
2023-02-14 15:27:49 -0500

    lessonp

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,8 +16,39 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source word?");
+        String sourceFileName = userInput.nextLine();
+
+
+        System.out.println("What is the destination word?");
+        String destinationFileName= userInput.nextLine();
+
+        File sourceFile = new File(sourceFileName);
+        File destinationFile = new File(destinationFileName);
+
+        try (Scanner fileScanner = new Scanner(sourceFile);
+             PrintWriter writer = new PrintWriter(destinationFile)) {
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+
+                if (line.contains(searchWord)) {
+                    line = line.replaceAll(searchWord, replacementWord);
+
+                }
+                writer.println(line);
+            }
+        }
+            catch (Exception ex) {
+                System.out.println("Something went wrong");
+            }
+        }
 
 
     }
-
-}
