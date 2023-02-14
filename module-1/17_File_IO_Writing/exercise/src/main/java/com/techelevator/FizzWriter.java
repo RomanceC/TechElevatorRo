6788e3a3d0b7428a8f145a455cb9f9a4ec04e387
Romance Cox <heyycurlie@gmail.com>
2023-02-14 16:17:21 -0500

    lessona

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
@@ -8,14 +10,38 @@ public class FizzWriter {
 	private final Scanner userInput = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		FizzWriter fizzWriter = new FizzWriter();
-		fizzWriter.run();
+
+		Scanner userScan = new Scanner(System.in);
+		System.out.println("Please enter a destination file ");
+		String myPath = userScan.nextLine();
+
+		File fizzBuzzFile = new File(myPath);
+		try (PrintWriter writer = new PrintWriter(fizzBuzzFile)) {
+			for (int number = 1; number <= 300; number++) {
+
+				if (number % 3 == 0 && number % 5 == 0) {
+					writer.println("FizzBuzz");
+				} else if (number % 3 == 0) {
+					writer.println("Fizz");
+				} else if (number % 5 == 0) {
+					writer.println("Buzz");
+				} else {
+					writer.println(number);
+				}
+			}
+
+		} catch (Exception exception) {
+			System.out.println("Oops! Sorry, we are having technical issues! Please try again!");
+
+		}
 	}
-
-	public void run() {
-		/* Your code goes here */
-
-
-	}
-
 }
+
+
+
+
+
+
+
+
+
