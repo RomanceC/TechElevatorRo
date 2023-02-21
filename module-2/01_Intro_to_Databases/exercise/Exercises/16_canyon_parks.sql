ea679e8b2c440809b666668fd4b5b37b03c02ccb
Romance Cox <heyycurlie@gmail.com>
2023-02-21 16:27:33 -0500

    lesson2

--- module-2/01_Intro_to_Databases/exercise/Exercises/16_canyon_parks.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/16_canyon_parks.sql
@@ -1,2 +1,4 @@
 -- 16. The name, date established, and area of parks that contain the string "Canyon" anywhere in the name (5 rows)
-
+SELECT park_name, date_established, area
+from Park
+WHERE park_name like '%Canyon%';
