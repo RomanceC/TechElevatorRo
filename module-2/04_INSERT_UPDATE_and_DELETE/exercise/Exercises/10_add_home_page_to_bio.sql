1cefc75d924eb857f3ed2ed665cadfa09268e318
Romance Cox <heyycurlie@gmail.com>
2023-02-24 16:29:16 -0500

    lesson

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_add_home_page_to_bio.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_add_home_page_to_bio.sql
@@ -1,3 +1,10 @@
 -- 10. For all people born before 1950 and have a home page, add the home page to the end of their biography. (142 rows)
 -- Note: Assume all biographies end in a period with no trailing spaces. You'll need to also add a space before the website address.
+SELECT person_name, birthday, biography
+FROM person
+WHERE home_page IS NOT null AND birthday < '1/1/1950';
+
+UPDATE person
+SET biography = (biography || ' '|| home_page)
+WHERE home_page IS NOT null AND birthday < '1/1/1950';
 
